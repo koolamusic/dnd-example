7e5b5c2c8f50f0f45a9fd0eebdcd40add747c2cb
Andrew Miracle <me@andrewmiracle.com>
2020-07-31 16:04:18 +0000

    setup dnd ordering in state

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { DragDropContext } from 'react-beautiful-dnd'
 import { initialData } from './initial-data'
 import Column from './column'
-// import './index.css'
+import './index.css'
 
 
 class App extends React.Component {
@@ -11,12 +11,52 @@ class App extends React.Component {
 
   onDragEnd = result => {
     console.log(result)
+    const { destination, source, draggableId } = result;
+
+    // check if user dumped outside dragContext
+    if (!destination) {
+      return
+    }
+
+    // also check if the user dumped the item at the index where they got it from 
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return;
+    }
+
+    //declare column from state based on droppableId
+    const column = this.state.columns[source.droppableId]
+    console.log("THIS IS COLL", column)
+    // create new TasksId Array based on states column
+    const newTaskIds = Array.from(column.taskIds)
+    // remove our source index and insert our new destination 
+    // index using array splice append the new id in second statement
+    newTaskIds.splice(source.index, 1)
+    newTaskIds.splice(destination.index, 0, draggableId)
+    console.log("NEW TASK IDS", newTaskIds)
+
+    // declare a new column based on the new column array and mutate our new taskIds. object spread
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+
+    // crete a new state based on newly derived results
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn
+      }
+    }
+    console.log("THIS IS NEW STATE", newState)
+    // call react setState with newState Object
+    this.setState(newState)
+
   }
 
   render() {
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
-
         {this.state.columnOrder.map(columnId => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
